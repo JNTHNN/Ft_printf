f216b39b1e3c04b114e37f9213cf9d83beb9cdc1
Jonathan Gasparoly <jgasparo@mi-r4-p3.s19.be>
2023-05-23 16:02:27 +0200

    23052023

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jgasparo <jgasparo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 12:34:45 by jgasparo          #+#    #+#             */
-/*   Updated: 2023/05/22 12:10:48 by jgasparo         ###   ########.fr       */
+/*   Updated: 2023/05/23 15:47:20 by jgasparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,27 @@ int	ft_printf(const char *format, ...)
 	i = -1;
 	while (format[++i])
 	{
+		if (format[i] != '%')
+			ft_putchar_fd(format[i], 1);
 		if (format[i] == '%')
 		{
-			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			i++;
+			if (format[i] == 'd' || format[i] == 'i')
 			{
 				int	d;
 				d = va_arg(args, int);
 				ft_putnbr_fd(d, 1);
 			}
-			if (format[i + 1] == 's')
+			else if (format[i] == 's')
 				ft_putstr_fd(va_arg(args, char *), 1);
+			else if (format[i] == '%')
+				ft_putchar_fd('%', 1);
+			else if (format[i] == 'c')
+				ft_putchar_fd(va_arg(args, int), 1);
+			else if (format[i] == 'u')
+				ft_putnbr_fd(va_arg(args, unsigned int), 1);
+			//else if (format[i] == 'p')
+			//	va_arg(args, void *)
 			}
 	}
 	va_end(args);
--- printf/main.c
+++ printf/main.c
@@ -3,15 +3,42 @@
 
 int main(int ac, char **av)
 {
-    ft_printf("%d et %s\n", -42, "ok cool");
+    /*ft_printf("ft_%d et %s\n", -42, "ok cool");
     printf("%d et %s", -42, "ok cool");
 
    printf ("Characters: %c %c \n", 'a', 65);
+   ft_printf ("ft_Characters: %c %c \n", 'a', 65);
    printf ("Decimals: %d %ld\n", 1977, 650000L);
+   ft_printf ("ft_Decimals: %d %ld\n", 1977, 650000L);
    printf ("Preceding with blanks: %10d \n", 1977);
+   ft_printf ("ft_Preceding with blanks: %10d \n", 1977);
    printf ("Preceding with zeros: %010d \n", 1977);
+   ft_printf ("ft_Preceding with zeros: %010d \n", 1977);
    printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+   ft_printf ("ft_Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
    printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+   ft_printf ("ft_floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
    printf ("Width trick: %*d \n", 5, 10);
+   ft_printf ("ft_Width trick: %*d \n", 5, 10);
    printf ("%s \n", "A string");
+   ft_printf ("%s \n", "ft_A string");
+*/
+   printf("1 Char : %c %c\n", 'a', 19);
+   ft_printf("ft_1 Char : %c %c\n", 'a', 19);
+   printf("1 string : %s\n", "l'ecole c'est cool");
+   ft_printf("ft_1 string : %s\n", "l'ecole c'est cool");
+   int  i = 19;
+   void *s = "Norman";
+   printf("Pointer : %p\n", (char *)s);
+   ft_printf("ft_Pointer : %p\n", (char *)s);
+   printf("Int : %d %i\n", 19, 19);
+   ft_printf("ft_Int : %d %i\n", 19, 19);
+   printf("Unsigned Int : %u\n", 19);
+   ft_printf("ft_Unsigned Int : %u\n", 19);
+   printf("Hexa min MAJ : %x %X\n", i, i);
+   ft_printf("ft_Hexa min MAJ : %x %X\n", i, i);
+   printf("Pourcent : %%\n");
+   ft_printf("ft_Pourcent : %%\n");
+
+   
 }
